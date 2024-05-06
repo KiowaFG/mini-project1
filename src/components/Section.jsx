1b9df9b4f40ed896067b06c10863dc97c3d6d896
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-05-06 18:02:06 +0200

    section formatted

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,29 +1,35 @@
 import "./Section.css";
+import { useState } from "react";
 import apartment_data from "../data/project_data.json";
-import starIcon from "../assets/images/star.png"
+import starIcon from "../assets/images/star.png";
+import heartFilledIcon from "../assets/images/filledheart_icon.png";
+import heartIcon from "../assets/images/heart_icon.png";
 
-const apartment_data_test = apartment_data.results.slice(0,19)
+const apartment_data_test = apartment_data.results
 
 const Section = () => {
     return (
         <div className="major-container">
             {
                 apartment_data_test.map((apartment) => {
+                    const [favState, setFavState] = useState(false);
+                    const setFavStateFunction = () => setFavState(!favState);
                     return (
                         <div key={apartment.id} className="main-container">
                             <img className="image-portfolio" src={apartment.picture_url.url} alt="test" />
+                            <img onClick={setFavStateFunction} className="fav-icon" src={favState ? heartFilledIcon : heartIcon} alt="test" />
                             {/* <img src="" alt="" />  We need to add bookmark icon/image here*/}
                             <div className="lower-container">
                                 <div className="lower2-container">
-                                    <h2>{apartment.city}, {apartment.country}</h2>
-                                    <h3>{apartment.neighbourhood}</h3>
-                                    <h3>Host: {apartment.host_name}</h3>
-                                    <h4>Max capacity: {apartment.accommodates} people</h4>
-                                    <h4>{apartment.price}€ night</h4>
+                                    <h5>{`${apartment.property_type} in ${apartment.neighbourhood ? apartment.neighbourhood : "La Latina"}`}</h5>
+                                    <p className="card-info">{`${apartment.city}, ${apartment.country}`}</p>
+                                    {/* <p>Host: {apartment.host_name}</p> */}
+                                    <p className="card-info">Max capacity: {apartment.accommodates} people</p>
+                                    <p className="priceCard-info"><span className="bold">€{apartment.price}</span> night</p>
                                 </div>
                                 <div className="lower3-container">
                                     <img className="rate-icon" src={starIcon} alt="" />
-                                    <h4>{apartment.review_scores_rating}/100</h4>
+                                    <p className="rating-info">{apartment.review_scores_rating ? apartment.review_scores_rating/10 : `N/A`}</p>
                                 </div>
                             </div>
                         </div>
