e625ee2b155e022071512d98b58701ec3353a753
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-05-06 22:41:17 +0200

    bookmarks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react'
+import { Route, Routes } from "react-router-dom";
 import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import Section from './components/Section'
+import apartment_data from "./data/project_data.json";
+import Favorites from './components/Favorites';
 
 function App() {
+  const [dataArray, setDataArray] = useState(apartment_data.results)
+  const [favArray, setFavArray] = useState([]);
+  const [searchArray, setSearchArray] = useState([]);
   return (
     <>
       <Navbar />
-      <Section />
+      <Routes>
+        <Route path="/properties" element={<Section dataArray={dataArray} favArray={favArray} setFavArray={setFavArray} />}></Route>
+        <Route path="/favorites" element={<Favorites favArray={favArray} setFavArray={setFavArray} />}></Route>
+      </Routes>
       <Footer />
     </>
   )
