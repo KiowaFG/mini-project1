48ecf55010018f51b4d4b250b81f4fa9a16061ef
KiowaFG <kiowacdq@gmail.com>
2024-05-08 18:59:03 +0200

    details

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,36 +7,35 @@ import search from "../assets/images/icons8-búsqueda-50.png"
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <Link to={"/"}>
-      <img className='logo' src={logo} alt="logo-img" />
-      </Link>
-      <ul>
-        <Link to="/">
-        <li>Home </li></Link>
-        <Link to="/properties">
-        <li className='dropdown'>Properties
-          <div className='dropdownContent'>
-            <a href="#">Studios</a>
-            <a href="#">Apartments</a>
-            <a href="#">Houses</a>
-            <a href="#">Vilas</a>
-          </div>
-        </li>
-        </Link>
-        <Link to="/favorites"><li>Favorites</li></Link>
-        <li>About</li>
-      </ul>
-      <div className='searchbar'>
+    return (
+        <div className='navbar'>
+            <Link to={"/"}>
+                <img className='logo' src={logo} alt="logo-img" />
+            </Link>
+            <ul>
+                <Link to="/">
+                    <li>Home </li></Link>
+                <li className='dropdown'><Link to="/properties">Properties </Link>
+                    <div className='dropdownContent'>
+                        <a href="#">Studios</a>
+                        <a href="#">Apartments</a>
+                        <a href="#">Houses</a>
+                        <a href="#">Vilas</a>
+                    </div>
+                </li>
+                <Link to="/favorites"><li>Favorites</li></Link>
+                <li>Add apartment</li>
+                <li>About</li>
+            </ul>
+            {/* <div className='searchbar'>
         <input type="text" placeholder='Search' />
         <img src={search} alt="" />
-      </div>
+      </div> */}
 
-      <img className='toggle-icon' src={moon} alt="" />
+            <img className='toggle-icon' src={moon} alt="" />
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Navbar
--- src/pages/ApartmentDetails.jsx
+++ src/pages/ApartmentDetails.jsx
@@ -1,11 +1,17 @@
 import { useParams } from "react-router-dom";
 import apartment_data from "../data/project_data.json";
+import Details from "../components/Details";
+import "./ApartmentListing.css"
 
 const ApartmentDetails = () => {
-    const {apartmentId} = useParams()
+    const { apartmentId } = useParams()
     const apartmentDetail = apartment_data.results.find(element => element.id === apartmentId)
     console.log(apartmentDetail);
-    return 
+    return (
+        <div className='upper-container'>
+            <Details apartmentDetail={apartmentDetail} />
+        </div>
+    )
 }
 
 export default ApartmentDetails;
