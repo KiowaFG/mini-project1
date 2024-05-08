cea763fec344efda105c28c8f61ff0ce7da45e0c
Ironhack <ironhack@MacBook-Air-de-Ironhack.local>
2024-05-08 15:51:03 +0200

    progres homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import Section from './components/Section'
 import HomePage from './components/HomePage'
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Section />
-      {/* <HomePage/> */}
+      <Routes>
+      
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/section" element={<Section/>} />
+      </Routes>
+
       <Footer />
     </>
   )
